868fbd31b614ab2c260ea4816d4bf99253550b5f
Hans-Peter Herzog <hans-peter.herzog@cdn-hph.de>
2016-03-24 23:48:22 +0100

    First steps

--- test/burst-spec.js
+++ test/burst-spec.js
@@ -5,13 +5,28 @@ import {BurstTimer} from '../lib/burst-timer';
 
 describe('BurstTimer', function(){
 
-    it('should start properly', function(done){
+    var burstTimer;
 
-        var burstTimer = new BurstTimer();
+    beforeEach(function(){
+        burstTimer = new BurstTimer();
+    });
+
+    it('should start properly', function(done) {
+
+        this.timeout(20000);
+
+        var counter = 0;
+
         burstTimer.onBurst(()=>{
-            
+            counter = counter + 1;
+
+            console.log(burstTimer);
+
+            if(counter === 10) {
+                done();
+            }
         });
+
         burstTimer.start();
-        done();
     });
 });
